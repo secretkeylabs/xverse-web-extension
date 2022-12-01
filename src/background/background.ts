b9c7b2ca93037106b7051a049eeea2025c66d1cc
Mahmoud <mahmoud@secretKeylabs.com>
2022-12-01 10:42:41 +0200

    fix for background port disconnecting

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,6 +1,3 @@
-// This file is the entrypoint to the extension's background script
-// https://developer.chrome.com/docs/extensions/mv3/architecture-overview/#background_script
-
 import { CONTENT_SCRIPT_PORT } from '../content-scripts/message-types';
 import type { LegacyMessageFromContentScript } from '../content-scripts/message-types';
 import {
@@ -16,6 +13,8 @@ chrome.runtime.onConnect.addListener((port) => {
   port.onMessage.addListener((message: LegacyMessageFromContentScript, port) => {
     if (inferLegacyMessage(message)) {
       void handleLegacyExternalMethodFormat(message, port);
+      // eslint-disable-next-line no-useless-return
+      return;
     }
   });
 });
