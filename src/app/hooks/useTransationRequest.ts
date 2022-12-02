c95658817ff7db56d69d8fa31dfe121a1251f6e8
Imamah-Zafar <88320460+Imamah-Zafar@users.noreply.github.com>
2022-12-02 11:30:36 +0500

    parse data

--- src/app/hooks/useTransationRequest.ts
+++ src/app/hooks/useTransationRequest.ts
@@ -1,14 +1,22 @@
-import { TransactionPayload } from '@stacks/connect';
+import { createContractCallPromises } from '@screens/transactionRequest/helper';
+
 import { decodeToken } from 'jsontokens';
 import { useLocation } from 'react-router-dom';
+import useWalletSelector from './useWalletSelector';
 
-const useDappRequest = () => {
+const useDappRequest = async () => {
+  const { network, stxAddress, stxPublicKey } = useWalletSelector();
   const { search } = useLocation();
   const params = new URLSearchParams(search);
   const requestToken = params.get('request') ?? '';
   const request = decodeToken(requestToken);
+  const [unSignedContractCall, contractInterface, coinsMetaData, showPostConditionMessage] = await createContractCallPromises(request.payload, stxAddress, network, stxPublicKey);
   return {
-    payload: request.payload as unknown as TransactionPayload,
+    payload: request.payload,
+    unSignedContractCall,
+    contractInterface,
+    coinsMetaData,
+    showPostConditionMessage,
   };
 };
 
---
+++ src/app/screens/transactionRequest/helper.ts
@@ -0,0 +1,125 @@
+import {
+  SettingsNetwork,
+  fetchStxPendingTxData,
+  getCoinsInfo,
+  generateUnsignedContractCall,
+  getNonce,
+  getNewNonce,
+  setNonce,
+} from '@secretkeylabs/xverse-core';
+import { ContractInterfaceResponse } from '@stacks/stacks-blockchain-api-types';
+import {
+  deserializeCV,
+  BufferReader,
+  deserializeStacksMessage,
+  StacksMessageType,
+  PostCondition,
+  addressToString,
+  PostConditionType,
+  FungiblePostCondition,
+} from '@stacks/transactions';
+import axios from 'axios';
+import { createContext } from 'react';
+
+function removeHexPrefix(hexString: string): string {
+  if (hexString !== 'string') return hexString;
+  return hexString.startsWith('0x') ? hexString.replace('0x', '') : hexString;
+}
+
+export function hexStringToBuffer(hex: string): Buffer {
+  return Buffer.from(removeHexPrefix(hex), 'hex');
+}
+
+// These helper functions are not only specific to contract function calls
+// They should be moved to the core lib
+
+export const extractFromPayload = (payload: any) => {
+  const { functionArgs, postConditions } = payload;
+  const funcArgs = functionArgs?.map((arg: string) => deserializeCV(hexStringToBuffer(arg)));
+
+  const postConds = Array.isArray(postConditions)
+    ? (postConditions?.map(
+      (arg: string) => deserializeStacksMessage(
+        new BufferReader(hexStringToBuffer(arg)),
+        StacksMessageType.PostCondition,
+      ) as PostCondition,
+    ) as PostCondition[])
+    : [];
+
+  return { funcArgs, postConds };
+};
+
+export const getFTInfoFromPostConditions = (postConds: PostCondition[]) => (
+  postConds?.filter(
+    (postCond) => postCond.conditionType === PostConditionType.Fungible,
+  ) as FungiblePostCondition[]
+)?.map(
+  (postCond: FungiblePostCondition) => `${addressToString(postCond.assetInfo.address)}.${postCond.assetInfo.contractName.content}`,
+);
+
+export const ShowMoreContext = createContext({ showMore: false });
+
+export async function getContractInterface(
+  contractAddress: string,
+  contractName: string,
+  network: SettingsNetwork,
+): Promise<ContractInterfaceResponse | null> {
+  const apiUrl = `${network.address}/v2/contracts/interface/${contractAddress}/${contractName}`;
+
+  return axios
+    .get<ContractInterfaceResponse>(apiUrl, {
+    timeout: 30000,
+  })
+    .then((response) => response.data)
+    .catch((error) => null);
+}
+
+export const createContractCallPromises = async (
+  payload: any,
+  stxAddress: string,
+  network: SettingsNetwork,
+  stxPublicKey: string,
+) => {
+  const sponsored = payload?.sponsored;
+  const { pendingTransactions } = await fetchStxPendingTxData(stxAddress, network);
+  const { funcArgs, postConds } = extractFromPayload(payload);
+
+  const ftContactAddresses = getFTInfoFromPostConditions(postConds);
+
+  const coinsMetaDataPromise = getCoinsInfo(ftContactAddresses, 'USD');
+
+  const tx = {
+    publicKey: stxPublicKey,
+    contractAddress: payload.contractAddress,
+    contractName: payload.contractName,
+    functionName: payload.functionName,
+    functionArgs: funcArgs,
+    network,
+    nonce: undefined,
+    postConditions: postConds,
+    sponsored,
+    postConditionMode: payload.postConditionMode,
+  };
+
+  const unSignedContractCall = await generateUnsignedContractCall(tx);
+  const { fee } = unSignedContractCall.auth.spendingCondition;
+
+  const checkForPostConditionMessage = payload?.postConditionMode === 2 && payload?.postConditions?.values.length <= 0;
+  const showPostConditionMessage = !!checkForPostConditionMessage;
+
+  const newNonce = getNewNonce(pendingTransactions, getNonce(unSignedContractCall));
+  setNonce(unSignedContractCall, newNonce);
+
+  const contractInterfacePromise = getContractInterface(
+    payload.contractAddress,
+    payload.contractName,
+    network,
+  );
+
+  return Promise.all([
+    unSignedContractCall,
+    contractInterfacePromise,
+    coinsMetaDataPromise,
+    showPostConditionMessage,
+  ]);
+};
