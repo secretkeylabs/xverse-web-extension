b5385252b2ef9d7759c8b1b656c8dc79639365eb
Mahmoud <mahmoud@secretKeylabs.com>
2022-12-21 15:55:44 +0200

    ui animation for landing and login screens

--- src/app/screens/login/index.tsx
+++ src/app/screens/login/index.tsx
@@ -3,24 +3,27 @@ import logo from '@assets/img/full_logo_vertical.svg';
 import styled from 'styled-components';
 import Eye from '@assets/img/createPassword/Eye.svg';
 import EyeSlash from '@assets/img/createPassword/EyeSlash.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Ring } from 'react-spinners-css';
 import useWalletReducer from '@hooks/useWalletReducer';
+import { animated, useSpring } from '@react-spring/web';
 
 declare const VERSION: string;
 
-const ScreenContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  padding-left: 18px;
-  padding-right: 18px;
-  overflow-y: auto;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
+const ScreenContainer = styled(animated.div)({
+  display: 'flex',
+  flexDirection: 'column',
+  flex: 1,
+  paddingLeft: 18,
+  paddingRight: 18,
+});
+
+const ContentContainer = styled(animated.div)({
+  display: 'flex',
+  flexDirection: 'column',
+  flex: 1,
+});
 
 const AppVersion = styled.p((props) => ({
   ...props.theme.body_xs,
@@ -109,6 +112,17 @@ function Login(): JSX.Element {
   const [password, setPassword] = useState<string>('');
   const [error, setError] = useState<string>('');
   const [isVerifying, setIsVerifying] = useState(false);
+  const styles = useSpring({
+    from: {
+      opacity: 0,
+      y: 24,
+    },
+    to: {
+      y: 0,
+      opacity: 1,
+    },
+    delay: 100,
+  });
 
   const handleTogglePasswordView = () => {
     setIsPasswordVisible(!isPasswordVisible);
@@ -132,6 +146,19 @@ function Login(): JSX.Element {
     }
   };
 
+  useEffect(() => {
+    const keyDownHandler = (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        handleVerifyPassword();
+      }
+    };
+    document.addEventListener('keydown', keyDownHandler);
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, [password]);
+
   const handleForgotPassword = () => {
     navigate('/forgotPassword');
   };
@@ -139,29 +166,32 @@ function Login(): JSX.Element {
   return (
     <ScreenContainer>
       <AppVersion>Beta version</AppVersion>
-      <TopSectionContainer>
-        <img src={logo} width={100} alt="logo" />
-        <LandingTitle>{t('WELCOME_MESSAGE_FIRST_LOGIN')}</LandingTitle>
-      </TopSectionContainer>
-      <PasswordInputLabel>{t('PASSWORD_INPUT_LABEL')}</PasswordInputLabel>
-      <PasswordInputContainer>
-        <PasswordInput
-          type={isPasswordVisible ? 'text' : 'password'}
-          value={password}
-          onChange={handlePasswordChange}
-          placeholder={t('PASSWORD_INPUT_PLACEHOLDER')}
-        />
-        <button type="button" onClick={handleTogglePasswordView} style={{ background: 'none' }}>
-          <img src={isPasswordVisible ? Eye : EyeSlash} alt="show-password" height={24} />
-        </button>
-      </PasswordInputContainer>
-      {error && <ErrorMessage>{error}</ErrorMessage>}
-      <VerifyButton onClick={handleVerifyPassword}>
-        {!isVerifying ? t('VERIFY_PASSWORD_BUTTON') : <Ring color="white" size={20} />}
-      </VerifyButton>
-      <ForgotPasswordButton onClick={handleForgotPassword}>
-        {t('FORGOT_PASSWORD_BUTTON')}
-      </ForgotPasswordButton>
+      <ContentContainer style={styles}>
+        <TopSectionContainer>
+          <img src={logo} width={100} alt="logo" />
+          <LandingTitle>{t('WELCOME_MESSAGE_FIRST_LOGIN')}</LandingTitle>
+        </TopSectionContainer>
+        <PasswordInputLabel>{t('PASSWORD_INPUT_LABEL')}</PasswordInputLabel>
+        <PasswordInputContainer>
+          <PasswordInput
+            type={isPasswordVisible ? 'text' : 'password'}
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder={t('PASSWORD_INPUT_PLACEHOLDER')}
+            autoFocus
+          />
+          <button type="button" onClick={handleTogglePasswordView} style={{ background: 'none' }}>
+            <img src={isPasswordVisible ? Eye : EyeSlash} alt="show-password" height={24} />
+          </button>
+        </PasswordInputContainer>
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+        <VerifyButton onClick={handleVerifyPassword}>
+          {!isVerifying ? t('VERIFY_PASSWORD_BUTTON') : <Ring color="white" size={20} />}
+        </VerifyButton>
+        <ForgotPasswordButton onClick={handleForgotPassword}>
+          {t('FORGOT_PASSWORD_BUTTON')}
+        </ForgotPasswordButton>
+      </ContentContainer>
     </ScreenContainer>
   );
 }
