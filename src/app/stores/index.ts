b7ec723c466c12469258a23035c55b73fc88c206
Mahmoud <mahmoud@secretKeylabs.com>
2023-02-09 00:33:06 +0200

    removed saga and updated redux-store setup

--- src/app/stores/index.ts
+++ src/app/stores/index.ts
@@ -1,9 +1,7 @@
 import ChromeStorage from '@utils/storage';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { persistReducer, persistStore } from 'redux-persist';
-import createSagaMiddleware from 'redux-saga';
-import walletReducer from './wallet/walletReducer';
-import rootSaga from './root/saga';
+import walletReducer from './wallet/reducer';
 import NftDataStateReducer from './nftData/reducer';
 
 export const storage = new ChromeStorage(chrome.storage.local, chrome.runtime);
@@ -17,7 +15,7 @@ const rootPersistConfig = {
 const WalletPersistConfig = {
   key: 'walletState',
   storage,
-  blacklist: ['seedPhrase', 'hasRestoredMemoryKey', 'configPrivateKey'],
+  blacklist: ['seedPhrase'],
 };
 
 const appReducer = combineReducers({
@@ -32,9 +30,7 @@ const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 export type StoreState = ReturnType<typeof rootReducer>;
 
 const rootStore = (() => {
-  const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
-  sagaMiddleware.run(rootSaga);
+  const store = createStore(persistedReducer);
   const persistedStore = persistStore(store);
   return { store, persistedStore };
 })();
