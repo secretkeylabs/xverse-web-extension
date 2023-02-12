34ee2793f459254e9f453bc143b9b5493c4b4fa7
Mahmoud <mahmoud@secretKeylabs.com>
2023-02-12 12:15:19 +0200

    fix btc data fetch error

--- src/app/hooks/queries/useBtcWalletData.ts
+++ src/app/hooks/queries/useBtcWalletData.ts
@@ -5,19 +5,14 @@ import { fetchBtcTransactionsData } from '@secretkeylabs/xverse-core/api';
 import { BtcAddressData } from '@secretkeylabs/xverse-core/types';
 import { SetBtcWalletDataAction } from '@stores/wallet/actions/actionCreators';
 import useWalletSelector from '../useWalletSelector';
-import useNetworkSelector from '../useNetwork';
 
 export const useBtcWalletData = () => {
   const dispatch = useDispatch();
-  const { btcAddress } = useWalletSelector();
-  const currentNetworkInstance = useNetworkSelector();
+  const { btcAddress, network } = useWalletSelector();
 
   const fetchBtcWalletData = async () => {
     try {
-      const btcData: BtcAddressData = await fetchBtcTransactionsData(
-        btcAddress,
-        currentNetworkInstance
-      );
+      const btcData: BtcAddressData = await fetchBtcTransactionsData(btcAddress, network.type);
       const btcBalance = new BigNumber(btcData.finalBalance);
       dispatch(SetBtcWalletDataAction(btcBalance, btcData.transactions));
       return btcData;
