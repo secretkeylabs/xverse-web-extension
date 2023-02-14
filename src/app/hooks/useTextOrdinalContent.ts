2b85a1d90b9684f2789d4c26c4af4e948307c906
Mahmoud <mahmoud@secretKeylabs.com>
2023-02-14 23:03:37 +0200

    fix ordinal text loading

--- src/app/hooks/useTextOrdinalContent.ts
+++ src/app/hooks/useTextOrdinalContent.ts
@@ -12,7 +12,7 @@ const useTextOrdinalContent = (ordinal: OrdinalInfo) => {
         const response = await getTextOrdinalContent(url); setTextContent(response ?? '');
       }
     })();
-  }, []);
+  }, [ordinal]);
 
   return textContent.toString();
 };
