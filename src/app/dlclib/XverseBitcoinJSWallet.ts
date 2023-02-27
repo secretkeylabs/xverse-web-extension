fcebf42b976395495a2dfdabcd70a6bae4944ef2
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2023-02-27 17:37:03 +0100

    Created Wallet and Blockchain classes

---
+++ src/app/dlclib/XverseBitcoinJSWallet.ts
@@ -0,0 +1,28 @@
+import { BitcoinJSWallet } from 'dlc-lib';
+import { Network, Transaction } from 'bitcoinjs-lib';
+import { Blockchain } from 'dlc-lib';
+import { WalletStorage } from 'dlc-lib';
+import { useSelector } from 'react-redux';
+import { StoreState } from '@stores/index';
+import { getBtcPrivateKey, NetworkType } from '@secretkeylabs/xverse-core';
+
+export class XverseBitcoinJSWallet extends BitcoinJSWallet {
+  constructor(storage: WalletStorage, network: Network, blockchain: Blockchain) {
+    super(storage, network, blockchain);
+  }
+
+  async getNewAddress(): Promise<string> {
+    const { btcAddress } = useSelector((state: StoreState) => state.walletState);
+    return btcAddress;
+  }
+  async getNewPublicKey(): Promise<string> {
+    const { btcPublicKey } = useSelector((state: StoreState) => state.walletState);
+    return btcPublicKey;
+  }
+  async getPrivateKey(index: BigInt): Promise<string> {
+    const network = 'Testnet';
+    const { seedPhrase } = useSelector((state: StoreState) => state.walletState);
+    const btcPrivateKey = getBtcPrivateKey({ seedPhrase, index, network });
+    return btcPrivateKey;
+  }
+}
