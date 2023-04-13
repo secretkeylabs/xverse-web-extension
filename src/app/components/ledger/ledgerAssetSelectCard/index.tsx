2ff71ac15a5547c93af384d152a3c381fd644c43
JPatrik <patrik.jokhel@dakai.io>
2023-04-13 15:50:49 +0200

    feat: add ledgerAssetSelectCard component

---
+++ src/app/components/ledger/ledgerAssetSelectCard/index.tsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+const CardContainer = styled.label((props) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  gap: props.theme.spacing(6),
+  justifyContent: 'flex-start',
+  padding: '14px 16px',
+  cursor: 'pointer',
+  background:
+    props.className === 'checked' ? props.theme.colors.background.selectBackground : 'transparent',
+  borderRadius: props.theme.radius(2),
+  border: `1px solid ${
+    props.className === 'checked' ? props.theme.colors.border.select : props.theme.colors.white[900]
+  }`,
+}));
+
+const CardInput = styled.input((props) => ({
+  display: 'none',
+}));
+
+const CardTitle = styled.h3((props) => ({
+  ...props.theme.body_bold_m,
+}));
+
+const CardText = styled.p((props) => ({
+  ...props.theme.body_medium_m,
+  color: props.theme.colors.white[400],
+}));
+
+interface Props {
+  icon: string;
+  title: string;
+  text: string;
+  id: string;
+  isChecked: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+function LedgerAssetSelectCard({
+  icon,
+  title,
+  text,
+  id,
+  isChecked,
+  onChange: handleChange,
+}: Props) {
+  return (
+    <div>
+      <CardContainer htmlFor={id} className={isChecked ? 'checked' : ''}>
+        <img src={icon} alt={`${title} icon`} />
+        <div>
+          <CardTitle>{title}</CardTitle>
+          <CardText>{text}</CardText>
+        </div>
+      </CardContainer>
+      <CardInput id={id} type="checkbox" checked={isChecked} onChange={handleChange} />
+    </div>
+  );
+}
+
+export default LedgerAssetSelectCard;
