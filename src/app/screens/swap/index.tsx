356d570a069da140a4e9f02efce85e3dc594366d
Kyle Fang <zhigang1992@gmail.com>
2023-04-15 09:59:24 +0800

    feat(swap): add swap basic UI

--- src/app/screens/swap/index.tsx
+++ src/app/screens/swap/index.tsx
@@ -3,19 +3,51 @@ import TopRow from '@components/topRow';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import BottomBar from '@components/tabBar';
+import SwapTokenBlock from '@screens/swap/swapTokenBlock';
+import ArrowDown from '@assets/img/swap/arrow_swap.svg';
+import useCoinsData from '@hooks/queries/useCoinData';
 
-const Container = styled.div`
+const ScrollContainer = styled.div`
   display: flex;
   flex: 1;
+  flex-direction: column;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  margin-left: 5%;
+  margin-right: 5%;
 `;
+
+const Container = styled.div((props) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  rowGap: props.theme.spacing(8),
+  marginTop: props.theme.spacing(16),
+}));
+
+const DownArrow = styled.img((props) => ({
+  alignSelf: 'center',
+  width: props.theme.spacing(18),
+  height: props.theme.spacing(18),
+}));
 
 function SwapScreen() {
   const navigate = useNavigate();
   const { t } = useTranslation('translation', { keyPrefix: 'SWAP_SCREEN' });
+  const { data } = useCoinsData();
+  console.log(data?.sortedFtList);
+
   return (
     <>
       <TopRow title={t('SWAP')} onClick={() => navigate('/')} />
-      <Container />
+      <ScrollContainer>
+        <Container>
+          <SwapTokenBlock title={t('CONVERT')} />
+          <DownArrow src={ArrowDown} />
+          <SwapTokenBlock title={t('TO')} />
+        </Container>
+      </ScrollContainer>
       <BottomBar tab="dashboard" />
     </>
   );
