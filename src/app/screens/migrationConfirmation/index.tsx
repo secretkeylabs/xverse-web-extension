b5c7890f7e6a5d2cdfa6a4b6bea1fa047dca8f32
Mahmoud <mahmoud@secretKeylabs.com>
2023-06-07 19:13:09 +0300

    added success status

--- src/app/screens/migrationConfirmation/index.tsx
+++ src/app/screens/migrationConfirmation/index.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import ActionButton from '@components/button';
 import migrateCachedStorage from '@utils/migrate';
 import WarningIcon from '@assets/img/Warning.svg';
+import WarningStatus from '@assets/img/warningIcon.svg';
+import CheckCircle from '@assets/img/createWalletSuccess/CheckCircle.svg';
 
 const Container = styled.div`
   display: flex;
@@ -35,7 +39,6 @@ const OnBoardingContentContainer = styled.div((props) => ({
 const OnboardingTitle = styled.h1((props) => ({
   ...props.theme.body_bold_l,
   textAlign: 'center',
-  marginTop: 'auto',
 }));
 
 const SkipButtonContainer = styled.div((props) => ({
@@ -43,12 +46,25 @@ const SkipButtonContainer = styled.div((props) => ({
   width: '100%',
 }));
 
-function MigrationConfirmation(): JSX.Element {
-  const navigate = useNavigate();
+const Icon = styled.img((props) => ({
+  width: 88,
+  height: 88,
+  alignSelf: 'center',
+  marginTop: 'auto',
+  marginBottom: props.theme.spacing(8),
+}));
 
+function MigrationConfirmation(): JSX.Element {
+  const [hasFinishedMigrating, setHasFinishedMigrating] = useState(false);
+  const navigate = useNavigate();
+  const { t } = useTranslation('translation', { keyPrefix: 'CACHE_MIGRATION_SCREEN' });
   const handleConfirm = async () => {
     await migrateCachedStorage();
-    navigate('/');
+    setHasFinishedMigrating(true);
+  };
+
+  const onCloseTab = () => {
+    window.close();
   };
 
   const handleSkip = () => {
@@ -58,16 +74,30 @@ function MigrationConfirmation(): JSX.Element {
   return (
     <Container>
       <OnBoardingContentContainer>
-        <OnboardingTitle>
-          Your wallet data store needs to be updated in order to remain secure. Please make sure you
-          have your seed phrase backed up securely before moving forward.
-        </OnboardingTitle>
-        <ButtonsContainer>
-          <SkipButtonContainer>
-            <ActionButton text="Skip" onPress={handleSkip} warning src={WarningIcon} />
-          </SkipButtonContainer>
-          <ActionButton onPress={handleConfirm} text="Confirm" />
-        </ButtonsContainer>
+        <Icon src={!hasFinishedMigrating ? WarningStatus : CheckCircle} />
+        {!hasFinishedMigrating ? (
+          <OnboardingTitle>{t('TITLE')}</OnboardingTitle>
+        ) : (
+          <OnboardingTitle>{t('SUCCESS_TITLE')}</OnboardingTitle>
+        )}
+
+        {!hasFinishedMigrating ? (
+          <ButtonsContainer>
+            <SkipButtonContainer>
+              <ActionButton
+                text={t('SKIP_BUTTON')}
+                onPress={handleSkip}
+                warning
+                src={WarningIcon}
+              />
+            </SkipButtonContainer>
+            <ActionButton onPress={handleConfirm} text={t('CONFIRM_BUTTON')} />
+          </ButtonsContainer>
+        ) : (
+          <ButtonsContainer>
+            <ActionButton text={t('CLOSE_TAB')} onPress={onCloseTab} />
+          </ButtonsContainer>
+        )}
       </OnBoardingContentContainer>
     </Container>
   );
