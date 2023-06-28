5f4f5a5dec210db935b04f77d2dc2149e4f8a4e6
Tim Man <timothyc.man@gmail.com>
2023-06-28 18:30:40 +0800

    fix: refactor select currency logic to use reducer and unit test

--- src/app/screens/swap/useSwap.tsx
+++ src/app/screens/swap/useSwap.tsx
@@ -12,6 +12,12 @@ import { useNavigate } from 'react-router-dom';
 import { SwapConfirmationInput } from '@screens/swap/swapConfirmation/useConfirmSwap';
 import { AnchorMode, makeUnsignedContractCall, PostConditionMode } from '@stacks/transactions';
 
+// const noop = () => null;
+const isNotNull = <T extends any>(t: T | null | undefined): t is T => t != null;
+
+export type STXOrFungibleToken = 'STX' | FungibleToken;
+export type Side = 'from' | 'to';
+
 export type SwapToken = {
   name: string;
   image: ReactNode;
@@ -25,7 +31,7 @@ export type UseSwap = {
   isLoadingWalletData: boolean;
   selectedFromToken?: SwapToken;
   selectedToToken?: SwapToken;
-  onSelectToken: (token: 'STX' | FungibleToken, side: 'from' | 'to') => void;
+  onSelectToken: (token: STXOrFungibleToken, side: Side) => void;
   inputAmount: string;
   inputAmountInvalid?: boolean;
   onInputAmountChanged: (amount: string) => void;
@@ -40,6 +46,60 @@ export type UseSwap = {
   onSlippageChanged: (slippage: number) => void;
   minReceived?: number;
   onSwap?: () => void;
+};
+
+export type SelectedCurrencyState = {
+  to?: Currency;
+  from?: Currency;
+  prevTo?: Currency;
+  prevFrom?: Currency;
+};
+
+function updateOppositeCurrencyIfSameAsSelected(state, {newCurrency, side}) {
+  switch (side) {
+    case 'from':
+      if (state.to !== newCurrency) {
+        return state.to;
+      }
+      if (state.to === newCurrency && state.prevTo !== newCurrency) {
+        return state.prevTo;
+      }
+      return undefined;
+    case 'to':
+      if (state.from !== newCurrency) {
+        return state.from;
+      }
+      if (state.from === newCurrency && state.prevFrom !== newCurrency) {
+        return state.prevFrom;
+      }
+      return undefined;
+    default:
+      return state.to;
+  }
+}
+
+export const selectedTokenReducer: (
+  state: SelectedCurrencyState,
+  { newCurrency, side }: { newCurrency: Currency; side: Side },
+) => SelectedCurrencyState = (state, { newCurrency, side }) => {
+  switch (side) {
+    case 'from':
+      return {
+        ...state,
+        prevFrom: state.from,
+        from: newCurrency,
+        to: updateOppositeCurrencyIfSameAsSelected(state, { newCurrency, side })
+      };
+    case 'to':
+      return {
+        ...state,
+        prevTo: state.to,
+        to: newCurrency,
+        from: updateOppositeCurrencyIfSameAsSelected(state, { newCurrency, side })
+      };
+    default:
+      return state;
+  }
 };
 
 export function useSwap(): UseSwap {
@@ -61,8 +121,16 @@ export function useSwap(): UseSwap {
 
   const [inputAmount, setInputAmount] = useState('');
   const [slippage, setSlippage] = useState(0.04);
-  const [from, setFrom] = useState<Currency>();
-  const [to, setTo] = useState<Currency>();
+  // const [from, setFrom] = useState<Currency | undefined>();
+  // const [to, setTo] = useState<Currency | undefined>();
+  // const [prevFrom, setPrevFrom] = useState<Currency | undefined>();
+  // const [prevTo, setPrevTo] = useState<Currency | undefined>();
+  const [selectedCurrency, setSelectedCurrency] = useState<SelectedCurrencyState>({
+    to: undefined,
+    from: undefined,
+    prevTo: undefined,
+    prevFrom: undefined,
+  });
 
   const fromAmount = Number.isNaN(Number(inputAmount)) ? undefined : Number(inputAmount);
 
@@ -73,7 +141,7 @@ export function useSwap(): UseSwap {
     if (currency === Currency.STX) {
       return {
         balance: Number(microstacksToStx(BigNumber(stxAvailableBalance) as any)),
-        image: <TokenImage round token="STX" size={28} loaderSize={LoaderSize.SMALL} />,
+        image: <TokenImage token="STX" size={28} loaderSize={LoaderSize.SMALL} />,
         name: 'STX',
         amount,
         fiatAmount:
@@ -90,9 +158,7 @@ export function useSwap(): UseSwap {
     }
     return {
       amount,
-      image: (
-        <TokenImage round fungibleToken={token} size={28} loaderSize={LoaderSize.SMALL} />
-      ),
+      image: <TokenImage fungibleToken={token} size={28} loaderSize={LoaderSize.SMALL} />,
       name: (token.ticker ?? token.name).toUpperCase(),
       balance: Number(ftDecimals(token.balance, token.decimals ?? 0)),
       fiatAmount:
@@ -115,13 +181,16 @@ export function useSwap(): UseSwap {
     return (token.ticker ?? token.name).toUpperCase();
   }
 
-  function onSelectToken(token: 'STX' | FungibleToken, side: 'from' | 'to') {
-    (side === 'from' ? setFrom : setTo)(
-      token === 'STX' ? Currency.STX : alexSDK.getCurrencyFrom(token.principal)!,
-    );
+  function getCurrencyFromToken(token: 'STX' | FungibleToken) {
+    return token === 'STX' ? Currency.STX : alexSDK.getCurrencyFrom(token.principal)!;
   }
 
-  const fromToken = currencyToToken(from, fromAmount);
+  function onSelectToken(token: STXOrFungibleToken, side: Side) {
+    const newCurrency = getCurrencyFromToken(token);
+    setSelectedCurrency(selectedTokenReducer(selectedCurrency, { newCurrency, side }));
+  }
+
+  const fromToken = currencyToToken(selectedCurrency.from, fromAmount);
   const inputAmountInvalid =
     Number.isNaN(Number(inputAmount)) ||
     (fromAmount != null &&
@@ -133,11 +202,18 @@ export function useSwap(): UseSwap {
   }>();
 
   useEffect(() => {
-    if (from == null || to == null || from === to) {
+    if (
+      selectedCurrency.from == null ||
+      selectedCurrency.to == null ||
+      selectedCurrency.from === selectedCurrency.to
+    ) {
       setInfo(undefined);
     } else {
       let cancelled = false;
-      Promise.all([alexSDK.getFeeRate(from, to), alexSDK.getRouter(from, to)]).then((a) => {
+      Promise.all([
+        alexSDK.getFeeRate(selectedCurrency.from, selectedCurrency.to),
+        alexSDK.getRouter(selectedCurrency.from, selectedCurrency.to),
+      ]).then((a) => {
         if (cancelled) {
           return;
         }
@@ -150,26 +226,38 @@ export function useSwap(): UseSwap {
         cancelled = true;
       };
     }
-  }, [from, to]);
+  }, [selectedCurrency.from, selectedCurrency.to]);
 
   const [exchangeRate, setExchangeRate] = useState<number>();
 
   useEffect(() => {
-    if (from == null || to == null || fromAmount == null || fromAmount === 0 || from === to) {
+    if (
+      selectedCurrency.from == null ||
+      selectedCurrency.to == null ||
+      fromAmount == null ||
+      fromAmount === 0 ||
+      selectedCurrency.from === selectedCurrency.to
+    ) {
       setExchangeRate(undefined);
     } else {
       let cancelled = false;
-      alexSDK.getAmountTo(from, BigInt(Math.floor(fromAmount * 1e8)), to).then((result) => {
-        if (cancelled) {
-          return;
-        }
-        setExchangeRate(Number(result) / 1e8 / fromAmount);
-      });
+      alexSDK
+        .getAmountTo(
+          selectedCurrency.from,
+          BigInt(Math.floor(fromAmount * 1e8)),
+          selectedCurrency.to,
+        )
+        .then((result) => {
+          if (cancelled) {
+            return;
+          }
+          setExchangeRate(Number(result) / 1e8 / fromAmount);
+        });
       return () => {
         cancelled = true;
       };
     }
-  }, [from, to, fromAmount]);
+  }, [selectedCurrency.from, selectedCurrency.to, fromAmount]);
 
   function roundForDisplay(input?: number) {
     if (input == null) {
@@ -178,16 +266,17 @@ export function useSwap(): UseSwap {
     return Math.floor(input * 1000) / 1000;
   }
 
-  const toggleFromToTokens = () => {
-    const prevFrom = from;
-    setFrom(to);
-    setTo(prevFrom);
+  function toggleFromToTokens() {
+    setSelectedCurrency((prevState) => ({
+      to: prevState.from,
+      from: prevState.to,
+    }));
   }
 
   const toAmount =
     exchangeRate != null && fromAmount != null ? fromAmount * exchangeRate : undefined;
 
-  const toToken = currencyToToken(to, roundForDisplay(toAmount));
+  const toToken = currencyToToken(selectedCurrency.to, roundForDisplay(toAmount));
   return {
     coinsList: acceptableCoinList,
     isLoadingWalletData: false,
@@ -214,12 +303,16 @@ export function useSwap(): UseSwap {
     },
     submitError: inputAmountInvalid ? t('ERRORS.INSUFFICIENT_BALANCE_FEES') : undefined,
     onSwap:
-      fromAmount != null && toAmount != null && from != null && to != null && info != null
+      fromAmount != null &&
+      toAmount != null &&
+      selectedCurrency.from != null &&
+      selectedCurrency.to != null &&
+      info != null
         ? async () => {
-            const tx = await alexSDK.runSwap(
+            const tx = alexSDK.runSwap(
               stxAddress,
-              from,
-              to,
+              selectedCurrency.from!,
+              selectedCurrency.to!,
               BigInt(Math.floor(fromAmount * 1e8)),
               BigInt(Math.floor(toAmount * (1 - slippage) * 1e8)),
               info.route,
@@ -236,15 +329,16 @@ export function useSwap(): UseSwap {
             });
 
             const state: SwapConfirmationInput = {
-              from: from!,
-              to: to!,
+              from: selectedCurrency.from!,
+              to: selectedCurrency.to!,
               fromToken: fromToken!,
               toToken: toToken!,
               address: stxAddress,
               fromAmount: fromAmount!,
               minToAmount: toAmount! * (1 - slippage),
               lpFeeAmount: info.feeRate * fromAmount!,
-              lpFeeFiatAmount: currencyToToken(from!, info.feeRate * fromAmount!)?.fiatAmount,
+              lpFeeFiatAmount: currencyToToken(selectedCurrency.from!, info.feeRate * fromAmount!)
+                ?.fiatAmount,
               routers: info.route.map(currencyToToken).filter(isNotNull),
               unsignedTx,
               functionName: `${tx.contractName}\n${tx.functionName}`,
@@ -256,6 +350,3 @@ export function useSwap(): UseSwap {
         : undefined,
   };
 }
-
-const noop = () => null;
-const isNotNull = <T extends any>(t: T | null | undefined): t is T => t != null;
