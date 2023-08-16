73c6b932e10832a8d32ca37697219111e532d78b
Imamah-Zafar <imamah.zafar@tintash.com>
2023-08-16 18:21:51 +0500

    Get account current nonce from confirmed and pending transactions

--- src/app/screens/swap/useAlexSponsoredTransaction.ts
+++ src/app/screens/swap/useAlexSponsoredTransaction.ts
@@ -1,8 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { StacksTransaction } from '@secretkeylabs/xverse-core';
+import {
+  getConfirmedTransactions,
+  StacksTransaction,
+  StxTransactionListData,
+} from '@secretkeylabs/xverse-core';
 import { AlexSDK } from 'alex-sdk';
 import useStxPendingTxData from '@hooks/queries/useStxPendingTxData';
+import useWalletSelector from '@hooks/useWalletSelector';
+import useNetworkSelector from '@hooks/useNetwork';
 
 const useAlexSponsorSwapEnabledQuery = (alexSDK: AlexSDK) =>
   useQuery({
@@ -13,7 +19,11 @@ const useAlexSponsorSwapEnabledQuery = (alexSDK: AlexSDK) =>
 export const useAlexSponsoredTransaction = (userOverrideSponsorValue: boolean) => {
   const alexSDK = useRef(new AlexSDK()).current;
   const [isServiceRunning, setIsServiceRunning] = useState(false);
+  const [hasPendingTransactions, setHasPendingTransactions] = useState(false);
   const { error, data: isEnabled, isLoading } = useAlexSponsorSwapEnabledQuery(alexSDK);
+  const { stxAddress } = useWalletSelector();
+  const selectedNetwork = useNetworkSelector();
+  const { data: stxPendingTxData } = useStxPendingTxData();
 
   useEffect(() => {
     if (!isLoading && !error) {
@@ -24,17 +34,35 @@ export const useAlexSponsoredTransaction = (userOverrideSponsorValue: boolean) =
   const sponsorTransaction = async (signed: StacksTransaction) =>
     alexSDK.broadcastSponsoredTx(signed.serialize().toString('hex'));
 
-  const { data: stxPendingTxData } = useStxPendingTxData();
-  const currentNonce =
-    (stxPendingTxData?.pendingTransactions ?? []).reduce(
-      (maxNonce, transaction) => Math.max(maxNonce, transaction?.nonce ?? 0),
-      0,
-    ) + 1;
+  const getAccountCurrentNonce = async () => {
+    let nonce = 0;
+    const confirmedTransactions: StxTransactionListData = await getConfirmedTransactions({
+      stxAddress,
+      network: selectedNetwork,
+    });
 
-  const filteredStxPendingTxData = stxPendingTxData?.pendingTransactions.filter(
-    (transaction) => currentNonce - transaction?.nonce === 1,
-  );
-  const hasPendingTransactions = filteredStxPendingTxData?.length > 0;
+    if (confirmedTransactions && confirmedTransactions.transactionsList.length > 0) {
+      nonce = confirmedTransactions.transactionsList[0].nonce + 1;
+    }
+    const currentPendingNonce =
+      (stxPendingTxData?.pendingTransactions ?? []).reduce(
+        (maxNonce, transaction) => Math.max(maxNonce, transaction?.nonce ?? 0),
+        0,
+      ) + 1;
+
+    return Math.max(currentPendingNonce, nonce);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const currentNonce = await getAccountCurrentNonce();
+      const filteredStxPendingTxData = stxPendingTxData?.pendingTransactions.filter(
+        (transaction) => currentNonce - transaction?.nonce === 1,
+      );
+
+      setHasPendingTransactions(filteredStxPendingTxData?.length > 0);
+    })();
+  }, [stxPendingTxData]);
 
   return {
     isSponsored: userOverrideSponsorValue && isServiceRunning && !hasPendingTransactions,
