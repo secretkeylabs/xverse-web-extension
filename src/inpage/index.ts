ce45e8c52ca2fd100e0f0b38001385c9c2736958
victorkirov <victor.kirov@gmail.com>
2023-08-28 09:26:08 +0300

    add Xverse namespaced providers

--- src/inpage/index.ts
+++ src/inpage/index.ts
@@ -1,5 +1,24 @@
-import StacksMethodsProvider from './stacks.inpage';
-import SatsMethodsProvider from './sats.inpage';
+import { StacksProvider } from '@stacks/connect';
+import { BitcoinProvider } from 'sats-connect';
 
+import SatsMethodsProvider from './sats.inpage';
+import StacksMethodsProvider from './stacks.inpage';
+
+declare global {
+  interface Window {
+    XverseProviders: {
+      StacksProvider: StacksProvider;
+      BitcoinProvider: BitcoinProvider;
+    };
+  }
+}
+
+// we keep this in case implementors call the default providers
 window.StacksProvider = StacksMethodsProvider;
 window.BitcoinProvider = SatsMethodsProvider;
+
+// and add this
+window.XverseProviders = {
+  StacksProvider: StacksMethodsProvider,
+  BitcoinProvider: SatsMethodsProvider,
+};
