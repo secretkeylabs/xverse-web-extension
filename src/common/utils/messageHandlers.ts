da46af138e854d151003e215995cccd95cd811e3
Mahmoud <mahmoud@secretKeylabs.com>
2023-09-27 00:34:06 +0300

    deprecate internal message handler

--- src/common/utils/messageHandlers.ts
+++ src/common/utils/messageHandlers.ts
@@ -1,4 +1,3 @@
-import { InternalMethods } from '@common/types/message-types';
 import { BackgroundMessages } from 'common/types/messages';
 
 function validateMessagesAreFromExtension(sender: chrome.runtime.MessageSender) {
@@ -6,36 +5,14 @@ function validateMessagesAreFromExtension(sender: chrome.runtime.MessageSender) 
   return sender.url?.startsWith(chrome.runtime.getURL(''));
 }
 
-let inMemoryKey = '';
-
 async function internalBackgroundMessageHandler(
   message: BackgroundMessages,
   sender: chrome.runtime.MessageSender,
   sendResponse: (response?: any) => void,
 ) {
   if (!validateMessagesAreFromExtension(sender)) {
+    sendResponse();
     return;
-  }
-  switch (message.method) {
-    case InternalMethods.ShareInMemoryKeyToBackground: {
-      const { secretKey } = message.payload;
-      inMemoryKey = secretKey;
-      sendResponse();
-      break;
-    }
-
-    case InternalMethods.RequestInMemoryKeys: {
-      sendResponse(inMemoryKey);
-      break;
-    }
-
-    case InternalMethods.RemoveInMemoryKeys: {
-      inMemoryKey = '';
-      sendResponse();
-      break;
-    }
-    default:
-      sendResponse();
   }
   sendResponse();
 }
