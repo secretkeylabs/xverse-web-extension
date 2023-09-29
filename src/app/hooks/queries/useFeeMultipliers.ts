37078cfdd915395e79bf4c2f5f123247a6101d4b
Den <36603049+dhriaznov@users.noreply.github.com>
2023-09-29 08:50:25 +0300

    Handling fees with thresholds (#601)
    * Handling fees with thresholds
    
    * Add margin under the high fees warning
    
    * Change the high fees warning position for Ordinals & Brc-20 txs
    
    * Show the warning if the initial fee from transaction is greater than the threshold
    
    * Add high fee warning for one-step brc20 transfer
    
    * update copy
    
    ---------
    
    Co-authored-by: Yukan <yukanliao@gmail.com>

--- src/app/hooks/queries/useFeeMultipliers.ts
+++ src/app/hooks/queries/useFeeMultipliers.ts
@@ -1,15 +1,15 @@
 import { useDispatch } from 'react-redux';
 import { useQuery } from '@tanstack/react-query';
-import { FeesMultipliers } from '@secretkeylabs/xverse-core/types';
+import { AppInfo } from '@secretkeylabs/xverse-core/types';
 import { fetchAppInfo } from '@secretkeylabs/xverse-core/api';
 import { setFeeMultiplierAction } from '@stores/wallet/actions/actionCreators';
 
 export const useFeeMultipliers = () => {
   const dispatch = useDispatch();
 
-  const fetchFeeMultiplierData = async (): Promise<FeesMultipliers> => {
+  const fetchFeeMultiplierData = async (): Promise<AppInfo> => {
     try {
-      const response: FeesMultipliers = await fetchAppInfo();
+      const response: AppInfo = await fetchAppInfo();
       dispatch(setFeeMultiplierAction(response));
       return response;
     } catch (err) {
