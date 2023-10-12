8a3549c2b61a6f421f8d6ad0409d3f7e8d7ce7a8
Denys Hriaznov <hriaznov.dev@gmail.com>
2023-10-12 10:16:39 +0200

    Change the html tag and text color constant

--- src/app/screens/legalLinks/index.tsx
+++ src/app/screens/legalLinks/index.tsx
@@ -64,9 +64,9 @@ const SwitchContainer = styled.div((props) => ({
   fontSize: '0.875rem',
 }));
 
-const DataCollectionDescription = styled.div((props) => ({
+const DataCollectionDescription = styled.p((props) => ({
   ...props.theme.body_m,
-  color: props.theme.colors.white['200'],
+  color: props.theme.colors.white_200,
   marginTop: props.theme.spacing(32),
 }));
 
