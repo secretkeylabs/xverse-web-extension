9394cc2f8f14f2431947164e343d3b813312dae3
Imamah-Zafar <imamah.zafar@tintash.com>
2023-10-26 00:01:29 +0500

    Add loader for recover ordinal screen

--- src/app/screens/restoreFunds/restoreOrdinals/index.tsx
+++ src/app/screens/restoreFunds/restoreOrdinals/index.tsx
@@ -14,6 +14,7 @@ import { useMutation } from '@tanstack/react-query';
 import { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { MoonLoader } from 'react-spinners';
 import styled from 'styled-components';
 import OrdinalRow from './ordinalRow';
 
@@ -36,6 +37,13 @@ const Container = styled.div({
   marginLeft: 16,
   marginTop: 32,
   marginRight: 16,
+});
+
+const LoaderContainer = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flex: 1,
 });
 
 const ErrorText = styled.h1((props) => ({
@@ -121,33 +129,42 @@ function RestoreOrdinals() {
     });
   };
 
+  const showContent =
+    ordinals?.length === 0 ? (
+      <>
+        <RestoreFundTitle>{t('RESTORE_ORDINAL_SCREEN.NO_FUNDS')}</RestoreFundTitle>
+        <ButtonContainer>
+          <ActionButton text={t('RESTORE_ORDINAL_SCREEN.BACK')} onPress={handleOnCancelClick} />
+        </ButtonContainer>
+      </>
+    ) : (
+      <>
+        <RestoreFundTitle>{t('RESTORE_ORDINAL_SCREEN.DESCRIPTION')}</RestoreFundTitle>
+        {ordinals?.map((ordinal) => (
+          <OrdinalRow
+            isLoading={transferringOrdinalId === ordinal.id}
+            disableTransfer={isLoading}
+            handleOrdinalTransfer={onClickTransfer}
+            ordinal={ordinal}
+            key={ordinal.id}
+          />
+        ))}
+        <ErrorContainer>
+          <ErrorText>{error}</ErrorText>
+        </ErrorContainer>
+      </>
+    );
+
   return (
     <>
       <TopRow title={t('RESTORE_ORDINAL_SCREEN.TITLE')} onClick={handleOnCancelClick} />
       <Container>
-        {ordinals?.length === 0 ? (
-          <>
-            <RestoreFundTitle>{t('RESTORE_ORDINAL_SCREEN.NO_FUNDS')}</RestoreFundTitle>
-            <ButtonContainer>
-              <ActionButton text={t('RESTORE_ORDINAL_SCREEN.BACK')} onPress={handleOnCancelClick} />
-            </ButtonContainer>
-          </>
+        {!ordinals ? (
+          <LoaderContainer>
+            <MoonLoader color="white" size={25} />
+          </LoaderContainer>
         ) : (
-          <>
-            <RestoreFundTitle>{t('RESTORE_ORDINAL_SCREEN.DESCRIPTION')}</RestoreFundTitle>
-            {ordinals?.map((ordinal) => (
-              <OrdinalRow
-                isLoading={transferringOrdinalId === ordinal.id}
-                disableTransfer={isLoading}
-                handleOrdinalTransfer={onClickTransfer}
-                ordinal={ordinal}
-                key={ordinal.id}
-              />
-            ))}
-            <ErrorContainer>
-              <ErrorText>{error}</ErrorText>
-            </ErrorContainer>
-          </>
+          showContent
         )}
       </Container>
       <BottomTabBar tab="nft" />
