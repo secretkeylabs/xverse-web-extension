8d1e249b9328ab691779504eb7d767b3eb930471
Imamah-Zafar <imamah.zafar@tintash.com>
2023-10-30 17:15:59 +0500

    Update NFT Detail UI

--- src/app/screens/nftDetail/nftAttribute.tsx
+++ src/app/screens/nftDetail/nftAttribute.tsx
@@ -2,12 +2,11 @@ import styled from 'styled-components';
 
 const Container = styled.h1((props) => ({
   display: 'flex',
-  borderRadius: 20,
+  borderRadius: 12,
   border: `1px solid ${props.theme.colors.elevation3}`,
-  flexDirection: 'row',
-  marginEnd: 10,
-  padding: props.theme.spacing(5),
-  alignItems: 'center',
+  flexDirection: 'column',
+  padding: '8px 16px',
+  alignItems: 'flex-start',
 }));
 
 const TypeText = styled.h1((props) => ({
@@ -19,7 +18,6 @@ const ValueText = styled.h1((props) => ({
   ...props.theme.body_medium_m,
   color: props.theme.colors.white_0,
   wordBreak: 'break-all',
-  marginLeft: props.theme.spacing(3),
 }));
 
 interface Props {
---
+++ src/app/screens/nftDetail/useNftDetail.ts
@@ -0,0 +1,104 @@
+import useNftDataSelector from '@hooks/stores/useNftDataSelector';
+import useNftDataReducer from '@hooks/stores/useNftReducer';
+import useResetUserFlow from '@hooks/useResetUserFlow';
+import useWalletSelector from '@hooks/useWalletSelector';
+import { getNftDetail, NftDetailResponse } from '@secretkeylabs/xverse-core';
+import { NftData } from '@secretkeylabs/xverse-core/types/api/stacks/assets';
+import { useMutation } from '@tanstack/react-query';
+import { GAMMA_URL } from '@utils/constants';
+import { getExplorerUrl, isLedgerAccount } from '@utils/helper';
+import { useEffect, useMemo, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+export default function useNftDetail() {
+  const navigate = useNavigate();
+  const { stxAddress, selectedAccount } = useWalletSelector();
+  const { id } = useParams();
+  const nftIdDetails = id!.split('::');
+  const { nftData } = useNftDataSelector();
+  const { storeNftData } = useNftDataReducer();
+  const [nft, setNft] = useState<NftData | undefined>(undefined);
+  const {
+    isLoading,
+    data: nftDetailsData,
+    mutate,
+  } = useMutation<NftDetailResponse | undefined, Error, { principal: string }>({
+    mutationFn: async ({ principal }) => {
+      const contractInfo: string[] = principal.split('.');
+      return getNftDetail(nftIdDetails[2].replace('u', ''), contractInfo[0], contractInfo[1]);
+    },
+  });
+  const isGalleryOpen: boolean = useMemo(() => document.documentElement.clientWidth > 360, []);
+
+  useResetUserFlow('/nft-detail');
+
+  useEffect(() => {
+    const data = nftData.find(
+      (nftItem) => Number(nftItem?.token_id) === Number(nftIdDetails[2].slice(1)),
+    );
+    if (!data) {
+      mutate({ principal: nftIdDetails[0] });
+    } else {
+      setNft(data);
+    }
+  }, []);
+
+  const onSharePress = () => {
+    navigator.clipboard.writeText(
+      `${GAMMA_URL}collections/${nft?.token_metadata?.contract_id}/${nft?.token_id}`,
+    );
+  };
+
+  useEffect(() => {
+    if (nftDetailsData) {
+      storeNftData(nftDetailsData.data);
+      setNft(nftDetailsData?.data);
+    }
+  }, [nftDetailsData]);
+
+  const handleBackButtonClick = () => {
+    navigate('/nft-dashboard?tab=nfts');
+  };
+
+  const onGammaPress = () => {
+    window.open(`${GAMMA_URL}collections/${nft?.token_metadata?.contract_id}`);
+  };
+
+  const onExplorerPress = () => {
+    const address = nft?.token_metadata?.contract_id?.split('.')!;
+    window.open(getExplorerUrl(address[0]));
+  };
+
+  const openInGalleryView = async () => {
+    await chrome.tabs.create({
+      url: chrome.runtime.getURL(`options.html#/nft-dashboard/nft-detail/${id}`),
+    });
+  };
+
+  const handleOnSendClick = async () => {
+    if (isLedgerAccount(selectedAccount)) {
+      await chrome.tabs.create({
+        url: chrome.runtime.getURL(`options.html#/send-nft/${id}`),
+      });
+      return;
+    }
+    navigate('send-nft', {
+      state: {
+        nft,
+      },
+    });
+  };
+
+  return {
+    nft,
+    stxAddress,
+    isLoading,
+    isGalleryOpen,
+    onSharePress,
+    handleBackButtonClick,
+    onGammaPress,
+    onExplorerPress,
+    openInGalleryView,
+    handleOnSendClick,
+  };
+}
