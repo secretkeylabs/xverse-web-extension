82e79223e940cd58f7db8331343220f2de28d0ec
Imamah-Zafar <imamah.zafar@tintash.com>
2023-10-31 19:40:33 +0500

    Add hook to get nft collection data

--- src/app/hooks/queries/useStacksCollectibles.ts
+++ src/app/hooks/queries/useStacksCollectibles.ts
@@ -1,6 +1,6 @@
 import useNetworkSelector from '@hooks/useNetwork';
 import useWalletSelector from '@hooks/useWalletSelector';
-import { getNfts, NftsListData } from '@secretkeylabs/xverse-core';
+import { getNftCollections, StacksCollectionList } from '@secretkeylabs/xverse-core';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { handleRetries, InvalidParamsError } from '@utils/query';
 
@@ -8,35 +8,29 @@ const useStacksCollectibles = () => {
   const { stxAddress } = useWalletSelector();
   const selectedNetwork = useNetworkSelector();
 
-  function fetchNfts({ pageParam = 0 }): Promise<NftsListData> {
+  function fetchNfts({ pageParam = 0 }): Promise<StacksCollectionList> {
     if (!stxAddress) {
       return Promise.reject(new InvalidParamsError('stxAddress is required'));
     }
-    return getNfts(stxAddress, selectedNetwork, pageParam);
+    return getNftCollections(stxAddress, selectedNetwork, pageParam || 0, 150);
   }
 
-  const { isLoading, data, fetchNextPage, isFetchingNextPage, hasNextPage, refetch, error } =
-    useInfiniteQuery([`nft-meta-data-${stxAddress}`], fetchNfts, {
-      retry: handleRetries,
-      keepPreviousData: false,
-      getNextPageParam: (lastpage, pages) => {
-        const currentLength = pages.map((page) => page.nftsList).flat().length;
-        if (currentLength < lastpage.total) {
-          return currentLength;
-        }
-        return false;
-      },
-    });
-
-  return {
-    isLoading,
-    error,
-    data,
-    fetchNextPage,
-    isFetchingNextPage,
-    hasNextPage,
-    refetch,
-  };
+  return useInfiniteQuery([`nft-collection-data-${stxAddress}`], fetchNfts, {
+    retry: handleRetries,
+    keepPreviousData: false,
+    getNextPageParam: (lastpage, pages) => {
+      const currentLength = pages
+        .map((page) => page.results)
+        .filter(Boolean)
+        .flat().length;
+      if (currentLength < lastpage.total) {
+        return currentLength;
+      }
+      return false;
+    },
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
 };
 
 export default useStacksCollectibles;
