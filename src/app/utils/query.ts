9f5aef668207a6406b85a1a2b6d744a64c206152
Tim Man <tim@secretkeylabs.com>
2023-11-09 20:17:01 +0800

    hotfix: react query cache fix (#643)
    * fix: use staleTime to control refetches
    
    and use defaults for refetchOnMount, refetchOnWindowFocus
    
    * release: v0.22.1

--- src/app/utils/query.ts
+++ src/app/utils/query.ts
@@ -15,6 +15,23 @@ export function handleRetries(failureCount: number, error: unknown): boolean {
   return failureCount < 3;
 }
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // time before garbage collection of query data
+      cacheTime: 5 * 60 * 1000, // 5 min
+
+      // increase this for specific queries to reduce refetches
+      staleTime: 0,
+
+      // at least one of these are required to refetch stale queries
+      // refetchInterval
+      // refetchIntervalInBackground
+      refetchOnMount: true,
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
+    },
+  },
+});
 
 export const offlineStorage = createSyncStoragePersister({ storage: window.localStorage });
