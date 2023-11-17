7749aefc9cccd970f739f53784c0d629cffd9572
Tim Man <tim@secretkeylabs.com>
2023-11-17 16:03:04 +0800

    refactor: remove unused utils function

--- src/app/utils/nfts.ts
+++ src/app/utils/nfts.ts
@@ -1,9 +1,4 @@
-import {
-  getBnsNftName,
-  NftData,
-  NonFungibleToken,
-  StacksCollectionData,
-} from '@secretkeylabs/xverse-core';
+import { getBnsNftName, NonFungibleToken, StacksCollectionData } from '@secretkeylabs/xverse-core';
 
 export const getNftsTabGridItemSubText = (collection: StacksCollectionData) =>
   collection.total_nft > 1 ? `${collection.total_nft} Items` : '1 Item';
@@ -11,6 +6,8 @@ export const getNftsTabGridItemSubText = (collection: StacksCollectionData) =>
 export const isBnsCollection = (collectionId?: string | null): boolean =>
   collectionId === 'SP000000000000000000002Q6VF78.bns';
 
+// fully_qualified_token_id like:
+// SP1E1RNN4JZ7T6Y0JVCSY2TH4918Z590P8JAB9HZM.radboy-first-feat::radboy-first-feat:64
 export const getFullyQualifiedKey = ({
   tokenId,
   contractName,
@@ -25,32 +22,6 @@ export const getIdentifier = (fullyQualifiedKey: string): NonFungibleToken['iden
     tokenId,
     contractName,
     contractAddress,
-  };
-};
-
-// fully_qualified_token_id like:
-// SP1E1RNN4JZ7T6Y0JVCSY2TH4918Z590P8JAB9HZM.radboy-first-feat::radboy-first-feat:64
-export const getNftDataFromNftsCollectionData = (
-  fully_qualified_token_id?: string,
-  nftCollections?: StacksCollectionData[],
-): {
-  collectionId: string;
-  collection?: StacksCollectionData;
-  nft?: NonFungibleToken;
-  nftData?: NftData;
-} => {
-  const collectionId = fully_qualified_token_id?.split('::')?.[0] ?? '';
-  const collection = nftCollections?.find((c) => c.collection_id === collectionId);
-  const nft = isBnsCollection(collectionId)
-    ? collection?.all_nfts[0] // assume bns collections have 1 item
-    : collection?.all_nfts.find(
-        (n: NonFungibleToken) => getFullyQualifiedKey(n.identifier) === fully_qualified_token_id,
-      );
-
-  return {
-    collectionId,
-    collection,
-    nft,
   };
 };
 
