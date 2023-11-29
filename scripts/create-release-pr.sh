baf34041958296d4e77af2a53f2afb0cdf048b9a
Tim Man <tim@secretkeylabs.com>
2023-11-29 22:33:06 +0800

    chore: fix script

--- scripts/create-release-pr.sh
+++ scripts/create-release-pr.sh
@@ -5,6 +5,8 @@ if [[ -z "$BUMP" ]]; then
   exit 1
 fi
 
+echo -e "\n\n--- Prepare for $BUMP release branch ---\n\n"
+
 git fetch --all
 git checkout develop
 git pull
@@ -12,14 +14,16 @@ git pull
 npm version $BUMP --git-tag-version=false
 VERSION=$(npm pkg get version | sed 's/"//g')
 TAG="v$VERSION"
-BRANCH=release/$TAG
-TITLE=release: $TAG
+BRANCH="release/$TAG"
+TITLE="release: $TAG"
 
-git checkout -b $BRANCH
+git checkout -B $BRANCH
 git commit -am "$TITLE"
 git merge origin/main -s ours
 
 git push --set-upstream origin $BRANCH
+
+echo -e "\n\n--- Create draft release for $TAG ---\n\n"
 
 gh api \                                                                                                                                                                                                                                                 ─╯
   --method POST \
@@ -36,18 +40,22 @@ gh api \                                                                        
 cat release.json | jq -r .body > body.md
 echo -e "\n\nDraft release: $(cat release.json | jq -r .html_url)" >> body.md
 
-for b in main develop; do 
+for b in main develop; do
+  echo -e "\n\n--- Create PR to $b ---\n\n"
+
   gh api \
     --method POST \
     -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     /repos/{owner}/{repo}/pulls \
-    -f title="$TITLE" \
+    -f title="$TITLE to $b" \
     -f body="Created by GitHub Actions Bot" \
-    -f head="$BRANCH to $b" \
+    -f head="$BRANCH" \
     -f base="$b" > pr-$b.json
 
-  $PR_ID=$(cat pr-$b.json | jq -r .id)
+  echo -e "\n\n--- Update PR to $b with description ---\n\n"
+
+  PR_ID=$(cat pr-$b.json | jq -r .number)
 
   gh api \
     --method PATCH \
@@ -60,6 +68,7 @@ for b in main develop; do
   # rm pr-$b.json
 done
 
+echo -e "\n\n--- Done ---\n\n"
 # clean up temp files
 # rm release.json
 # rm body.md
