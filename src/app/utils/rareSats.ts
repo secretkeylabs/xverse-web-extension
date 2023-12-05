8f321477dc5ca01c10809843cbc4b01592cd0ab4
Tim Man <tim@secretkeylabs.com>
2023-12-05 21:48:34 +0800

    revert: check out old component and hack to display v2 bundle data
    remember to revert this when working on sign psbt screen v2

--- src/app/utils/rareSats.ts
+++ src/app/utils/rareSats.ts
@@ -41,3 +41,69 @@ export const getSatLabel = (satributes: RareSatsType[]): string => {
     ? `${t('COMMON.COMBO')}`
     : `${getRareSatsLabelByType(satributes[0])} ${getRareSatsLabelByType(satributes[1])}`;
 };
+
+// remove later when we fix sign psbt screen
+export type BundleItem =
+  | {
+      type: 'rare-sat';
+      rarity_ranking: RodarmorRareSatsType;
+      number: string;
+    }
+  | {
+      type: 'inscribed-sat';
+      rarity_ranking: RodarmorRareSatsType;
+      number: string;
+      inscription: {
+        id: string;
+        content_type: string;
+      };
+    }
+  | {
+      type: 'inscription';
+      rarity_ranking: RodarmorRareSatsType;
+      inscription: {
+        id: string;
+        content_type: string;
+      };
+    }
+  | {
+      type: 'unknown';
+      rarity_ranking: 'unknown';
+    };
+
+// remove later when we fix sign psbt screen
+export const convertV2ToV1Bundle = (v2: any): BundleItem[] => {
+  console.log('v2:', v2);
+  const bundleItems: BundleItem[] = [];
+  v2.forEach((item) => {
+    item.satRanges.forEach((satRange) => {
+      satRange.inscriptions.forEach((inscription) => {
+        bundleItems.push({
+          type: 'inscription',
+          rarity_ranking: 'COMMON',
+          inscription,
+        });
+      });
+    });
+  });
+  return bundleItems;
+};
+
+// remove later when we fix sign psbt screen
+export const getBundleItemSubText = ({
+  satType,
+  rareSatsType,
+}: {
+  satType: any;
+  rareSatsType: RareSatsType;
+}) =>
+  ({
+    inscription: t('COMMON.INSCRIPTION'),
+    'rare-sat': t('RARE_SATS.SAT_TYPES.RARE_SAT', {
+      type: getRareSatsLabelByType(rareSatsType ?? 'unknown'),
+    }),
+    'inscribed-sat': t('RARE_SATS.SAT_TYPES.INSCRIBED_RARE_SAT', {
+      type: getRareSatsLabelByType(rareSatsType ?? 'unknown'),
+    }),
+    unknown: t('RARE_SATS.SAT_TYPES.UNKNOWN_RARE_SAT'),
+  }[satType]);
