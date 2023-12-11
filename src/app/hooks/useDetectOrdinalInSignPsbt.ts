13712b486580f8f500c0868d2eda00b6266cb1f1
Tim Man <tim@secretkeylabs.com>
2023-12-11 12:43:35 +0800

    fix: allow 404s when checking psbt utxos for ordinals

--- src/app/hooks/useDetectOrdinalInSignPsbt.ts
+++ src/app/hooks/useDetectOrdinalInSignPsbt.ts
@@ -4,6 +4,7 @@ import {
   mapRareSatsAPIResponseToBundle,
   ParsedPSBT,
 } from '@secretkeylabs/xverse-core';
+import { isAxiosError } from 'axios';
 import useWalletSelector from './useWalletSelector';
 
 export type InputsBundle = (Pick<Bundle, 'satRanges' | 'totalExoticSats'> & {
@@ -21,10 +22,21 @@ const useDetectOrdinalInSignPsbt = () => {
 
     if (parsedPsbt) {
       const inputsRequest = parsedPsbt.inputs.map((input) =>
-        getUtxoOrdinalBundle(network.type, input.txid, input.index),
+        getUtxoOrdinalBundle(network.type, input.txid, input.index).catch((e) => {
+          // we don't reject on 404s because if the UTXO is not found,
+          // it is likely this is a UTXO from an unpublished txn.
+          // this is required for gamma.io purchase flow
+          if (!isAxiosError(e) || e.response?.status !== 404) {
+            // rethrow error if response was not 404
+            throw e;
+          }
+        }),
       );
       const inputsResponse = await Promise.all(inputsRequest);
       inputsResponse.forEach((inputResponse, index) => {
+        if (!inputResponse) {
+          return;
+        }
         const bundle = mapRareSatsAPIResponseToBundle(inputResponse);
         if (
           bundle.inscriptions.length > 0 ||
