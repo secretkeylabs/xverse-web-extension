749ad9b581629644e9673d99f60c771e5d08fbe7
Abdul Haseeb <haseeb4239@gmail.com>
2024-01-05 14:23:33 +0500

    fix: back button

--- src/app/components/transactionSetting/index.tsx
+++ src/app/components/transactionSetting/index.tsx
@@ -303,6 +303,7 @@ function TransactionSettingAlert({
               onPress={() => {
                 setCustomFeeSelected(false);
               }}
+              transparent
             />
           </LeftButton>
           <RightButton>
