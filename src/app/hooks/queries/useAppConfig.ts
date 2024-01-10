d179a4c0fe2653d5c2ba5207153270bf681f8d65
Tim Man <tim@secretkeylabs.com>
2024-01-10 14:22:51 +0800

    fix: remove unused remote btcApiUrl config functionality

--- src/app/hooks/queries/useAppConfig.ts
+++ src/app/hooks/queries/useAppConfig.ts
@@ -1,24 +1,14 @@
 import useWalletSelector from '@hooks/useWalletSelector';
 import { getAppConfig } from '@secretkeylabs/xverse-core';
-import { ChangeNetworkAction } from '@stores/wallet/actions/actionCreators';
 import { useQuery } from '@tanstack/react-query';
-import { useDispatch } from 'react-redux';
 
 const useAppConfig = () => {
   const { network } = useWalletSelector();
-  const dispatch = useDispatch();
 
   return useQuery({
-    queryKey: ['app-config', network.type, network.btcApiUrl],
+    queryKey: ['app-config', network.type],
     queryFn: async () => {
       const response = await getAppConfig(network.type);
-
-      // was this ever used? we set defaults for btcApiUrl in useWalletSelector
-      if (response.data.btcApiURL && network.type === 'Mainnet' && !network.btcApiUrl) {
-        const updatedNetwork = { ...network, btcApiUrl: response.data.btcApiURL };
-        dispatch(ChangeNetworkAction(updatedNetwork));
-      }
-
       return response;
     },
   });
