a91cc151131740c7f861868e6b4efad2eebaf780
jordankzf <jordan@secretkeylabs.com>
2024-01-18 15:13:15 +0800

    Don't override existing token's visiblity setting

--- src/app/hooks/queries/useBtcCoinsBalance.ts
+++ src/app/hooks/queries/useBtcCoinsBalance.ts
@@ -6,17 +6,28 @@ import { useDispatch } from 'react-redux';
 
 const useBtcCoinBalance = () => {
   const dispatch = useDispatch();
-  const { ordinalsAddress, network } = useWalletSelector();
+  const { ordinalsAddress, network, brcCoinsList } = useWalletSelector();
 
   const fetchBrcCoinsBalances = async () => {
     try {
       const list = await getOrdinalsFtBalance(network.type, ordinalsAddress);
-      dispatch(
-        setBrcCoinsDataAction(
-          list.map((brcToken) => ({ ...brcToken, ticker: brcToken.ticker?.toUpperCase() })),
-        ),
-      );
-      return list;
+
+      const mergedList = list.map((newToken) => {
+        const existingToken = brcCoinsList?.find((et) => et.ticker === newToken.ticker);
+
+        return {
+          ...newToken,
+          // The old code enforces the ticker as upper case in the store, even though it returns the lower case ticker
+          // Removing this since BRC-20 tokens do not have contracts, and rely on the case-insensitive ticker as a unique identifier
+          // (it's case-sensitive within our code, because it's a string)
+          // ticker: newToken.ticker?.toUpperCase(),
+          // The `visible` property from `xverse-core` defaults to true.
+          // We override `visible` to ensure that the existing state is preserved.
+          ...(existingToken ? { visible: existingToken.visible } : {}),
+        };
+      });
+      dispatch(setBrcCoinsDataAction(mergedList));
+      return mergedList;
     } catch (e: any) {
       return Promise.reject(e);
     }
