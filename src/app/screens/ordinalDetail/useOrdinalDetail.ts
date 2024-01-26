4b4fd1536727d86d6f7cd9c24f4b8bf757794505
fede erbes <fedeerbes@gmail.com>
2024-01-26 13:56:33 +0000

    chore: getBrc20Details core util to detect if inscription is a valid brc20

--- src/app/screens/ordinalDetail/useOrdinalDetail.ts
+++ src/app/screens/ordinalDetail/useOrdinalDetail.ts
@@ -7,13 +7,14 @@ import useOrdinalDataReducer from '@hooks/stores/useOrdinalReducer';
 import useSatBundleDataReducer from '@hooks/stores/useSatBundleReducer';
 import useTextOrdinalContent from '@hooks/useTextOrdinalContent';
 import useWalletSelector from '@hooks/useWalletSelector';
+import { getBrc20Details } from '@secretkeylabs/xverse-core';
 import { XVERSE_ORDIVIEW_URL } from '@utils/constants';
 import { getBtcTxStatusUrl, isInOptions, isLedgerAccount } from '@utils/helper';
 import {
   getInscriptionsCollectionGridItemSubText,
   getInscriptionsCollectionGridItemSubTextColor,
 } from '@utils/inscriptions';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useTheme } from 'styled-components';
@@ -42,7 +43,6 @@ export default function useOrdinalDetail() {
   const { t } = useTranslation('translation', { keyPrefix: 'NFT_DETAIL_SCREEN' });
 
   const [showSendOridnalsAlert, setshowSendOridnalsAlert] = useState(false);
-  const [isBrc20Ordinal, setIsBrc20Ordinal] = useState(false);
 
   const isGalleryOpen: boolean = useMemo(() => document.documentElement.clientWidth > 360, []);
 
@@ -50,11 +50,10 @@ export default function useOrdinalDetail() {
   const brc20InscriptionStatusColor =
     theme.colors[getInscriptionsCollectionGridItemSubTextColor(ordinalData)];
 
-  useEffect(() => {
-    if (textContent?.includes('brc-20')) {
-      setIsBrc20Ordinal(true);
-    }
-  }, [textContent]);
+  const brc20Details = useMemo(
+    () => getBrc20Details(textContent ?? '', ordinalData?.content_type ?? ''),
+    [textContent, ordinalData?.content_type],
+  );
 
   const handleBackButtonClick = () => {
     setSelectedOrdinalDetails(null);
@@ -129,7 +128,7 @@ export default function useOrdinalDetail() {
     isLoading,
     ordinalsAddress,
     showSendOridnalsAlert,
-    isBrc20Ordinal,
+    isBrc20Ordinal: Boolean(brc20Details),
     isPartOfABundle: isPartOfABundle && hasActivatedRareSatsKey,
     ordinalSatributes: hasActivatedRareSatsKey ? ordinalSatributes : [],
     isGalleryOpen,
