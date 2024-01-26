ce8573563a9420a46c3082939c79ce320cdd3db4
Mahmoud Aboelenein <mahmoud@secretkeylabs.com>
2024-01-26 12:38:24 +0200

    resolve ui issues

--- src/app/screens/connect/selectAccount.tsx
+++ src/app/screens/connect/selectAccount.tsx
@@ -1,4 +1,3 @@
-import LedgerBadge from '@assets/img/ledger/ledger_badge.svg';
 import { CaretRight } from '@phosphor-icons/react';
 import { Account } from '@secretkeylabs/xverse-core';
 import { getAccountGradient } from '@utils/gradient';
@@ -17,23 +16,16 @@ const AccountInfoContainer = styled.button((props) => ({
   padding: props.theme.space.m,
 }));
 
-const CurrentAccountContainer = styled.div((props) => ({
+const CurrentAccountContainer = styled.div({
   display: 'flex',
   alignItems: 'center',
-}));
-
-const CurrentAccountTextContainer = styled.div((props) => ({
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  gap: props.theme.spacing(4),
-  paddingLeft: props.theme.spacing(4),
-}));
+});
 
 const CurrentSelectedAccountText = styled.h1((props) => ({
   ...props.theme.typography.body_medium_m,
   color: props.theme.colors.white_0,
   textAlign: 'start',
+  paddingLeft: props.theme.space.xs,
 }));
 
 interface GradientCircleProps {
@@ -46,6 +38,11 @@ const GradientCircle = styled.div<GradientCircleProps>((props) => ({
   height: 20,
   borderRadius: '50%',
   background: `linear-gradient(to bottom,${props.firstGradient}, ${props.secondGradient},${props.thirdGradient} )`,
+}));
+
+const AccountTag = styled.div(() => ({
+  display: 'flex',
+  alignItems: 'center',
 }));
 
 const SwitchAccountContainer = styled.div(() => ({
@@ -69,25 +66,30 @@ function SelectAccount({ account, handlePressAccount }: SelectAccountProps) {
   // const { t } = useTranslation('translation', { keyPrefix: 'DASHBOARD_SCREEN' });
   const { t } = useTranslation('translation', { keyPrefix: 'SELECT_BTC_ADDRESS_SCREEN' });
   const theme = useTheme();
-  const getName = () =>
-    account?.accountName ??
-    account?.bnsName ??
-    `${t('ACCOUNT_NAME')} ${`${(account?.id ?? 0) + 1}`}`;
+  const getName = () => {
+    const maxNameCharacters = isHardwareAccount(account) ? 12 : 20;
+    const maxLength =
+      account?.accountName && account?.accountName?.length > maxNameCharacters ? '....' : '';
+    if (account.accountName) {
+      return `${account?.accountName?.slice(0, 20)}${maxLength}`;
+    }
+    if (account.bnsName) {
+      return account.bnsName;
+    }
+    return `${t('ACCOUNT_NAME')} ${`${(account?.id ?? 0) + 1}`}`;
+  };
 
   return (
     <AccountInfoContainer onClick={handlePressAccount}>
       <CurrentAccountContainer>
-        <GradientCircle
-          firstGradient={gradient[0]}
-          secondGradient={gradient[1]}
-          thirdGradient={gradient[2]}
-        />
-        {account && (
-          <CurrentAccountTextContainer>
-            <CurrentSelectedAccountText>{getName()}</CurrentSelectedAccountText>
-            {isHardwareAccount(account) && <img src={LedgerBadge} alt="Ledger icon" />}
-          </CurrentAccountTextContainer>
-        )}
+        <AccountTag>
+          <GradientCircle
+            firstGradient={gradient[0]}
+            secondGradient={gradient[1]}
+            thirdGradient={gradient[2]}
+          />
+          {account && <CurrentSelectedAccountText>{getName()}</CurrentSelectedAccountText>}
+        </AccountTag>
       </CurrentAccountContainer>
       <SwitchAccountContainer>
         <SwitchAccountText>{t('CHANGE_ACCOUNT_BUTTON')}</SwitchAccountText>
