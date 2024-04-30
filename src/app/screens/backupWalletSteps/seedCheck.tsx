d4567daccaf8bc8e877d81a9c6c841b25c26df5f
Terence Ng <ngtere@live.com.sg>
2024-04-30 12:02:33 +0800

    Update onboarding seed phrase blurring (#163)
    * blurring revamp
    
    * touchup
    
    ---------
    
    Co-authored-by: Den <36603049+dhriaznov@users.noreply.github.com>

--- src/app/screens/backupWalletSteps/seedCheck.tsx
+++ src/app/screens/backupWalletSteps/seedCheck.tsx
@@ -1,4 +1,5 @@
 import SeedphraseView from '@components/seedPhraseView';
+import { Eye, EyeSlash } from '@phosphor-icons/react';
 import Button from '@ui-library/button';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -22,32 +23,45 @@ const Label = styled.p((props) => ({
   marginBottom: props.theme.spacing(4),
 }));
 
-const ContinueButton = styled(Button)((props) => ({
+const StyledButton = styled(Button)((props) => ({
   marginBottom: props.theme.space.xxxl,
+}));
+
+const ButtonContainer = styled.div((props) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  gap: props.theme.space.s,
 }));
 
 interface SeedCheckPros {
   onContinue: () => void;
   seedPhrase: string;
-  showButton?: boolean;
 }
 
 export default function SeedCheck(props: SeedCheckPros): JSX.Element {
   const { t } = useTranslation('translation', { keyPrefix: 'BACKUP_WALLET_SCREEN' });
-  const { onContinue, seedPhrase, showButton = true } = props;
+  const { onContinue, seedPhrase } = props;
   const [isVisible, setIsVisible] = useState<boolean>(false);
   return (
     <Container>
       <Heading>{t('SEED_PHRASE_VIEW_HEADING')}</Heading>
       <Label>{t('SEED_PHRASE_VIEW_LABEL')}</Label>
-      <SeedphraseView seedPhrase={seedPhrase} isVisible={isVisible} setIsVisible={setIsVisible} />
-      {showButton && (
-        <ContinueButton
+      <SeedphraseView seedPhrase={seedPhrase} isVisible={isVisible} />
+      <ButtonContainer>
+        <StyledButton
+          icon={isVisible ? <EyeSlash weight="bold" size={16} /> : <Eye weight="bold" size={16} />}
+          type="button"
+          variant="secondary"
+          title={isVisible ? t('SEED_PHRASE_VIEW_HIDE') : t('SEED_PHRASE_VIEW_REVEAL')}
+          onClick={() => setIsVisible(!isVisible)}
+        />
+        <StyledButton
           disabled={!isVisible}
           onClick={onContinue}
           title={t('SEED_PHRASE_VIEW_CONTINUE')}
         />
-      )}
+      </ButtonContainer>
     </Container>
   );
 }
