1e61040e1a15122a05ea1f8c7edc004ac13ccdaa
Den <36603049+dhriaznov@users.noreply.github.com>
2024-06-05 19:14:50 +0200

    [ENG-4429] Hide the Runes Recovery button for Ledger accounts (#321)

--- src/app/screens/restoreFunds/index.tsx
+++ src/app/screens/restoreFunds/index.tsx
@@ -3,6 +3,7 @@ import RuneIcon from '@assets/img/nftDashboard/rune_icon.svg';
 import BottomTabBar from '@components/tabBar';
 import TopRow from '@components/topRow';
 import useWalletSelector from '@hooks/useWalletSelector';
+import { isHardwareAccount } from '@utils/helper';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
@@ -26,7 +27,7 @@ const Container = styled.div({
 
 function RestoreFunds() {
   const { t } = useTranslation('translation', { keyPrefix: 'RESTORE_FUND_SCREEN' });
-  const { hasActivatedOrdinalsKey } = useWalletSelector();
+  const { hasActivatedOrdinalsKey, selectedAccount } = useWalletSelector();
   const navigate = useNavigate();
 
   const handleOnCancelClick = () => {
@@ -53,12 +54,15 @@ function RestoreFunds() {
           description={t('RECOVER_ORDINALS_DESC')}
           onClick={handleOnRestoreOrdinalClick}
         />
-        <FundsRow
-          image={RuneIcon}
-          title={t('RECOVER_RUNES')}
-          description={t('RECOVER_RUNES_DESC')}
-          onClick={onRecoverRunesClick}
-        />
+        {/* TODO: Fix the runes recovery for ledger and then enable it */}
+        {!isHardwareAccount(selectedAccount) && (
+          <FundsRow
+            image={RuneIcon}
+            title={t('RECOVER_RUNES')}
+            description={t('RECOVER_RUNES_DESC')}
+            onClick={onRecoverRunesClick}
+          />
+        )}
       </Container>
       <BottomTabBar tab="settings" />
     </>
