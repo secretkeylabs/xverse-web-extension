5ca4dab121f5abaead1bd45dd53d8adf2e062c1c
Eduard Bardají Puig <ebardaji@gmail.com>
2024-06-26 16:00:28 +0100

    Rename method

--- src/common/utils/rpc/wallet/handle-connect.ts
+++ src/common/utils/rpc/wallet/handle-connect.ts
@@ -1,14 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import { getTabIdFromPort } from '@common/utils';
 import { makeContext, openPopup } from '@common/utils/popup';
-import { RpcRequestMessage, connectSchema } from '@sats-connect/core';
+import { RpcRequestMessage, connectRequestMessageSchema } from '@sats-connect/core';
 import * as v from 'valibot';
 import RequestsRoutes from '../../route-urls';
 import { handleInvalidMessage } from '../handle-invalid-message';
 import { makeSendPopupClosedUserRejectionMessage } from '../helpers';
 
 export const handleConnect = async (message: RpcRequestMessage, port: chrome.runtime.Port) => {
-  const parseResult = v.safeParse(connectSchema, message);
+  const parseResult = v.safeParse(connectRequestMessageSchema, message);
 
   if (!parseResult.success) {
     handleInvalidMessage(message, getTabIdFromPort(port), parseResult.issues);
