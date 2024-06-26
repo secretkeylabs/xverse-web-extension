e31cce58ef25a89b76bcf36d5ee4671209428cca
Eduard Bardají Puig <ebardaji@gmail.com>
2024-06-26 15:57:10 +0100

    Set permissions on getRunesBalance

--- src/common/utils/rpc/runes/getBalance.ts
+++ src/common/utils/rpc/runes/getBalance.ts
@@ -1,10 +1,43 @@
+import { getTabIdFromPort } from '@common/utils';
 import getSelectedAccount from '@common/utils/getSelectedAccount';
-import { RpcErrorCode, RpcId } from '@sats-connect/core';
+import { makeContext } from '@common/utils/popup';
+import { makeAccountResourceId } from '@components/permissionsManager/resources';
+import * as utils from '@components/permissionsManager/utils';
+import {
+  RpcErrorCode,
+  RpcRequestMessage,
+  getRunesBalanceRequestMessageSchema,
+} from '@sats-connect/core';
 import { getRunesClient } from '@secretkeylabs/xverse-core';
 import rootStore from '@stores/index';
+import * as v from 'valibot';
+import { handleInvalidMessage } from '../handle-invalid-message';
 import { makeRPCError, makeRpcSuccessResponse, sendRpcResponse } from '../helpers';
+import {
+  sendAccessDeniedResponseMessage,
+  sendInternalErrorMessage,
+} from '../responseMessages/errors';
 
-const handleGetRunesBalance = async (requestId: RpcId, tabId: number) => {
+const handleGetRunesBalance = async (message: RpcRequestMessage, port: chrome.runtime.Port) => {
+  const parseResult = v.safeParse(getRunesBalanceRequestMessageSchema, message);
+
+  if (!parseResult.success) {
+    handleInvalidMessage(message, getTabIdFromPort(port), parseResult.issues);
+    return;
+  }
+  const { origin, tabId } = makeContext(port);
+  const [loadError, store] = await utils.loadPermissionsStore();
+
+  if (loadError) {
+    sendInternalErrorMessage({ tabId, messageId: message.id });
+    return;
+  }
+
+  if (!store) {
+    sendAccessDeniedResponseMessage({ tabId, messageId: message.id });
+    return;
+  }
+
   const {
     selectedAccountIndex,
     selectedAccountType,
@@ -12,6 +45,22 @@ const handleGetRunesBalance = async (requestId: RpcId, tabId: number) => {
     ledgerAccountsList,
     network,
   } = rootStore.store.getState().walletState;
+
+  const permission = utils.getClientPermission(
+    store.permissions,
+    origin,
+    makeAccountResourceId({ accountId: selectedAccountIndex, networkType: network.type }),
+  );
+
+  if (!permission) {
+    sendAccessDeniedResponseMessage({ tabId, messageId: message.id });
+    return;
+  }
+
+  if (!permission.actions.has('read')) {
+    sendAccessDeniedResponseMessage({ tabId, messageId: message.id });
+    return;
+  }
 
   const existingAccount = getSelectedAccount({
     selectedAccountIndex,
@@ -23,7 +72,7 @@ const handleGetRunesBalance = async (requestId: RpcId, tabId: number) => {
   if (!existingAccount) {
     sendRpcResponse(
       tabId,
-      makeRPCError(requestId, {
+      makeRPCError(message.id, {
         code: RpcErrorCode.INTERNAL_ERROR,
         message: 'Could not find selected account.',
       }),
@@ -37,7 +86,7 @@ const handleGetRunesBalance = async (requestId: RpcId, tabId: number) => {
     const runesBalances = await runesApi.getRuneBalances(existingAccount.ordinalsAddress);
     sendRpcResponse(
       tabId,
-      makeRpcSuccessResponse<'runes_getBalance'>(requestId, {
+      makeRpcSuccessResponse<'runes_getBalance'>(message.id, {
         balances: runesBalances.map((runeBalance) => ({
           ...runeBalance,
           amount: runeBalance.amount.toString(),
@@ -47,7 +96,7 @@ const handleGetRunesBalance = async (requestId: RpcId, tabId: number) => {
   } catch (error) {
     sendRpcResponse(
       tabId,
-      makeRPCError(requestId, {
+      makeRPCError(message.id, {
         code: RpcErrorCode.INTERNAL_ERROR,
         message: (error as any).message,
       }),
