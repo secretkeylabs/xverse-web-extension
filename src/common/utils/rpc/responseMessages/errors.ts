6b1ee1b7a79194a39230fa99d6f7270274357e1c
Eduard Bardají Puig <ebardaji@gmail.com>
2024-06-26 15:56:42 +0100

    Fix message

--- src/common/utils/rpc/responseMessages/errors.ts
+++ src/common/utils/rpc/responseMessages/errors.ts
@@ -105,7 +105,7 @@ export function sendAccessDeniedResponseMessage({ tabId, messageId }: BaseArgs) 
     tabId,
     makeRPCError(messageId, {
       code: RpcErrorCode.ACCESS_DENIED,
-      message: 'This .',
+      message: 'Access denied.',
     }),
   );
 }
