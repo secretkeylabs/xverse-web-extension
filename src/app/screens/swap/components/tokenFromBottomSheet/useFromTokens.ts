f3fb6472d1a5fb0d126a9418a4e473beef5de7d8
jordankzf <jordan@secretkeylabs.com>
2024-07-25 22:42:24 +0800

    Simplify useFromTokens even further

--- src/app/screens/swap/components/tokenFromBottomSheet/useFromTokens.ts
+++ src/app/screens/swap/components/tokenFromBottomSheet/useFromTokens.ts
@@ -1,8 +1,7 @@
 import { useRuneFungibleTokensQuery } from '@hooks/queries/runes/useRuneFungibleTokensQuery';
 import useCoinRates from '@hooks/queries/useCoinRates';
 import useSelectedAccount from '@hooks/useSelectedAccount';
-import { mapFTProtocolToSwapProtocol } from '@screens/swap/utils';
-import { type FungibleToken, type TokenBasic } from '@secretkeylabs/xverse-core';
+import { type FungibleToken } from '@secretkeylabs/xverse-core';
 import { sortFtByFiatBalance } from '@utils/tokens';
 
 const useFromTokens = () => {
@@ -15,32 +14,15 @@ const useFromTokens = () => {
     return acc;
   }, {} as Record<FungibleToken['principal'], FungibleToken>);
 
-  const runesBasicTokens =
-    Object.values(filteredRunesTokensObject).map((ft) => ({
-      ticker: ft.principal,
-      protocol: mapFTProtocolToSwapProtocol(ft.protocol ?? 'runes'),
-    })) ?? [];
-
-  const btcBasicToken: TokenBasic = { protocol: 'btc', ticker: 'BTC' };
-  const userTokens = [...(btcAddress ? [btcBasicToken] : [])].concat(runesBasicTokens);
-
-  const tokens = userTokens
-    .filter((token) => token.protocol === 'btc' || !!filteredRunesTokensObject[token.ticker])
-    .map((token) => {
-      if (token.protocol === 'btc') {
-        return 'BTC';
-      }
-      if (token.protocol === 'runes') {
-        return filteredRunesTokensObject[token.ticker];
-      }
-      return token;
-    })
-    .sort((a, b) => {
-      if (b === 'BTC') return 1;
-      const aFT = a as FungibleToken;
-      const bFT = b as FungibleToken;
+  const tokens: (FungibleToken | 'BTC')[] = [...Object.values(filteredRunesTokensObject)].sort(
+    (a, b) => {
+      const aFT = a;
+      const bFT = b;
       return sortFtByFiatBalance(aFT, bFT, stxBtcRate, btcFiatRate);
-    });
+    },
+  );
+
+  if (btcAddress) tokens.unshift('BTC');
 
   return tokens;
 };
