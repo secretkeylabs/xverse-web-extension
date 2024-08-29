94419635fdb464fbbf1d8ce739e094234edd4b1c
jordankzf <jordan@secretkeylabs.com>
2024-08-29 17:31:02 +0800

    Remove vite

--- vitest.config.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-  plugins: [tsconfigPaths()],
-});
