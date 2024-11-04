186d16d024e09e31f3a91a4d9361d29e6ba00adf
Victor Kirov <victor.kirov@gmail.com>
2024-11-04 17:14:41 +0200

    Remove adding locked stx balance to total balance (#748)

--- src/app/screens/home/balanceCard/index.tsx
+++ src/app/screens/home/balanceCard/index.tsx
@@ -93,9 +93,7 @@ function BalanceCard(props: BalanceCardProps) {
   const { data: runesCoinList } = useVisibleRuneFungibleTokens();
 
   const balance = calculateTotalBalance({
-    stxBalance: BigNumber(stxData?.balance ?? 0)
-      .plus(stxData?.locked ?? 0)
-      .toString(),
+    stxBalance: BigNumber(stxData?.balance ?? 0).toString(),
     btcBalance: (btcBalance ?? 0).toString(),
     sipCoinsList: sip10CoinsList,
     brcCoinsList: brc20CoinsList,
