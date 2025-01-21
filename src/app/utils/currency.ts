d664476db6efd502f930548c17401d62fe1c41b0
Terence Ng <ngtere@live.com.sg>
2025-01-21 15:02:49 +0800

    support more countries (#878)

--- src/app/utils/currency.ts
+++ src/app/utils/currency.ts
@@ -4,8 +4,11 @@ import BRA from '@assets/img/settings/currencies/bra.svg';
 import CAN from '@assets/img/settings/currencies/can.svg';
 import CHE from '@assets/img/settings/currencies/che.svg';
 import CHN from '@assets/img/settings/currencies/chn.svg';
+import COP from '@assets/img/settings/currencies/cop.svg';
+import DOP from '@assets/img/settings/currencies/dop.svg';
 import EU from '@assets/img/settings/currencies/eu.svg';
 import GBR from '@assets/img/settings/currencies/gbr.svg';
+import GTQ from '@assets/img/settings/currencies/gtq.svg';
 import HKG from '@assets/img/settings/currencies/hkg.svg';
 import HUN from '@assets/img/settings/currencies/hun.svg';
 import IDN from '@assets/img/settings/currencies/idn.svg';
@@ -16,8 +19,10 @@ import MEX from '@assets/img/settings/currencies/mex.svg';
 import MYS from '@assets/img/settings/currencies/mys.svg';
 import NGA from '@assets/img/settings/currencies/nga.svg';
 import PAK from '@assets/img/settings/currencies/pak.svg';
+import PEN from '@assets/img/settings/currencies/pen.svg';
 import PHL from '@assets/img/settings/currencies/phl.svg';
 import POL from '@assets/img/settings/currencies/pol.svg';
+import RON from '@assets/img/settings/currencies/ron.svg';
 import RUS from '@assets/img/settings/currencies/rus.svg';
 import SGP from '@assets/img/settings/currencies/sgp.svg';
 import THA from '@assets/img/settings/currencies/tha.svg';
@@ -63,4 +68,9 @@ export const currencyList: Currency[] = [
   { name: 'PKR', isoFlag: PAK }, // Pakistan
   { name: 'ZAR', isoFlag: ZAF }, // South Africa
   { name: 'MXN', isoFlag: MEX }, // Mexico
+  { name: 'RON', isoFlag: RON }, // Romania
+  { name: 'GTQ', isoFlag: GTQ }, // Guatemala
+  { name: 'COP', isoFlag: COP }, // Colombia
+  { name: 'DOP', isoFlag: DOP }, // Dominican Republic
+  { name: 'PEN', isoFlag: PEN }, // Peru
 ];
